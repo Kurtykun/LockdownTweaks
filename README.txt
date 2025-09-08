8fb47d8d746744d34e5947c93ced381f8483ca25
Kurtykun <Kurt_martin@live.co.uk>
2025-09-08 22:47:00 +0100

    Version 1.0
    Version 1.0
    * Removed the maintain sound. It still "plays" but the sound file is blank.
    * Added Neimans item to distribution tables.
    * Added .223 to 5.56 recipe. Some occupations start with the recipe learned and can make notes to share it.
    * Added "Corvus' Cookbook" which contains all the random recipes that dont fit anywhere else.
    * Added a more time efficient vinegar recipe. With an increased skill requirement and resource cost that is friendlier with extended day length.
    * Modified armour values of MILITARY PONCHOS from the Military Ponchos Expanded Mod. 
    * Herbal tea can be made in a wider variety of mugs. Now you can have tea in a spiffo mug!
    * BEANS! Can now be used in most recipes that would take tinned beans.
    * Reduced spawn rates of 4.6 and 5.7 by 50% to account for limited weapons which utilise it. (Testing.)
    * Added recipe to create a military poncho from a yellow tarp then recipes to paint it various colours.

---
+++ README.txt
@@ -0,0 +1,27 @@
+Version 1.0
+* Removed the maintain sound. It still "plays" but the sound file is blank.
+* Added Neimans item to distribution tables.
+* Added .223 to 5.56 recipe. Some occupations start with the recipe learned and can make notes to share it.
+* Added "Corvus' Cookbook" which contains all the random recipes that dont fit anywhere else.
+* Added a more time efficient vinegar recipe. With an increased skill requirement and resource cost that is friendlier with extended day length.
+* Modified armour values of MILITARY PONCHOS from the Military Ponchos Expanded Mod. 
+* Herbal tea can be made in a wider variety of mugs. Now you can have tea in a spiffo mug!
+* BEANS! Can now be used in most recipes that would take tinned beans.
+* Reduced spawn rates of 4.6 and 5.7 by 50% to account for limited weapons which utilise it. (Testing.)
+* Added recipe to create a military poncho from a yellow tarp then recipes to paint it various colours. 
+
+Full list of edited mods.
+
+Food Preservation Plus https://steamcommunity.com/sharedfiles/filedetails/?id=2890748284
+Herbal Medicine Plus https://steamcommunity.com/sharedfiles/filedetails/?id=2891494114
+For Science! https://steamcommunity.com/sharedfiles/filedetails/?id=2803794394
+Military Ponchos Expanded https://steamcommunity.com/sharedfiles/filedetails/?id=2858828646 
+More Lock - key and lock management system https://steamcommunity.com/sharedfiles/filedetails/?id=2738370718
+Simple Overhaul: Traits and Occupations (SOTO) https://steamcommunity.com/sharedfiles/filedetails/?id=2840805724
+VFE - Post '93 https://steamcommunity.com/sharedfiles/filedetails/?id=3007780511
+VFE - Post '93 - Unofficial Finish https://steamcommunity.com/sharedfiles/filedetails/?id=3477936480 
+
+
+Full credit to all mod authors thank you for you mods and for making this possible!
+
+This mod was made assistance of Microsoft Copilot.
---
+++ media/lua/server/SOTOProfessionTweaks.lua
@@ -0,0 +1,70 @@
+-- Adds ammo conversion recipes to specific professions in Project Zomboid
+-- Directly overrides profession definitions. 
+Events.OnInitGlobalModData.Add(function()
+    if isClient() and not isServer() then return end
+    
+    local professions = ProfessionFactory.getProfessions()
+    local recipesToAdd = {
+        "Convert .223 to 5.56",
+        "Convert box of .223 to 5.56", 
+        "Write .223 = 5.56 Note"
+    }
+    
+    for i = 0, professions:size() - 1 do
+        local prof = professions:get(i)
+        local profType = prof:getType()
+        
+        if profType == "policeofficer" or profType == "veteran" or 
+           profType == "soldier" or profType == "detective" then
+            
+            for _, recipe in ipairs(recipesToAdd) do
+                prof:getFreeRecipes():add(recipe)
+            end
+            print("[YourModName] Added recipes to profession definition: " .. profType)
+        end
+    end
+end)
+-- This code very likely isnt working as intended and should be tested on server and removed if not working.
+-- If working the profession will show in the recipes list. 
+
+-- Backup method which add recpie to players at runtime it doesnt override the mod it just adds the recipes to the player.
+Events.OnGameStart.Add(function()
+    local player = getPlayer()
+    if not player then return end
+
+    local prof = player:getDescriptor():getProfession()
+    local targetProf = {
+        policeofficer = true, 
+        veteran = true, 
+        soldier = true, 
+        detective = true
+    }
+    
+    if targetProf[prof] then
+        player:getKnownRecipes():add("Convert .223 to 5.56")
+        player:getKnownRecipes():add("Convert box of .223 to 5.56")
+        player:getKnownRecipes():add("Write .223 = 5.56 Note")
+        
+        print("[YourModName] Added ammo conversion recipes to player: " .. prof)
+    end
+end)
+
+Events.OnCreatePlayer.Add(function(playerIndex, player)
+    if not player then return end
+    
+    local prof = player:getDescriptor():getProfession()
+    local targetProf = {
+        policeofficer = true, 
+        veteran = true, 
+        soldier = true, 
+        detective = true
+    }
+    
+    if targetProf[prof] then
+        player:getKnownRecipes():add("Convert .223 to 5.56")
+        player:getKnownRecipes():add("Convert box of .223 to 5.56")
+        player:getKnownRecipes():add("Write .223 = 5.56 Note")
+        
+        print("[YourModName] Added ammo conversion recipes to joining player: " .. prof)
+    end
+end)
